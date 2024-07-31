5c1e981b4302b759a7042da3d9d8a3caa7381f3d
merytpeters <merytpeters@gmail.com>
2024-07-31 20:36:33 +0100

    Instruction for number

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -32,7 +32,7 @@ def python_text(text="is cool"):
     return f'Python {text}'
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     return f'{n} is a number'
 
