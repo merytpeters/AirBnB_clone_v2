47d3834e31b56befa3f9b2d6eda8478a9dd260f9
merytpeters <merytpeters@gmail.com>
2024-08-20 13:25:59 +0100

    Web flask that displays html

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -2,7 +2,7 @@
 """Script that starts a Flask web application"""
 
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -39,7 +39,7 @@ def number(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    return render_template('5-number.html')
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
